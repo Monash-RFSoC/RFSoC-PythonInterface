9a82bc7b2e20064c1758393ed2fad78cad8cde31
tave0002 <tave0002@student.monash.edu>
2026-02-11 15:04:58 +1100

    print_program functions correctly and add_instruction requires a resync and phase flag

--- RFBuilder/blocks/sources/pulse_blaster.py
+++ RFBuilder/blocks/sources/pulse_blaster.py
@@ -13,22 +13,23 @@ class PulseBlaster(Source):
                 "BRANCH":6,
                 "LONG_DELAY":7,
                 "WAIT":8}
+    instructionLength = 128 #in bits
     phaseWordBits = 30
     Fclk = 500 #MHz
-
     def __init__(self):
         self.instruction_list: list = []
         self.num_instructions: int = 0
+        super().__init__("pulseblaster", [Port(PortDirection.OUTPUT, 2)])
         self.custom_update = True
 
-
-        super().__init__("pulseblaster", [Port(PortDirection.OUTPUT, 2)])
-
-    def add_instruction(self,phaseWord: float,freqWord: float,ttlStates: int,dataField: int,opcode: str,delayCounter: int):
+    def add_instruction(self,phasehopFlag: bool,resyncFlag: bool,phaseWord: float,freqWord: float,ttlStates: int,dataField: int,opcode: str,delayCounter: int):
         """
-        Given the input parameters, generates a 128 bit wide instruction word (as a string) that can be read by the PBFSM.
-        Returns a tuple containing the instruction word, the rounded frequency, and the rounded phase.
+        Given the input parameters, create the 128 bit wide instruction and adds it to the program which can be sent to the pulse blaster.
 
+        :param phasehopFlag: Set to true if the frequency word should be used to perform a global phase hop.
+        :type phasehopFlag: bool
+        :param resyncFlag: Set to true if you want to reset the phase accumulation register of the DDS back to 0
+        :type resyncFlag: bool
         :param phaseWord: Desired phase offset in degrees, will be rounded based on input clock frequency
         :type phaseWord: float
         :param frequWord: Desired frequency in MHz, this will be converted to the required phase impliment to calculate that frequency
@@ -44,13 +45,13 @@ class PulseBlaster(Source):
         :param Fclk: The frequency that the DDS connected to the PBFSM runs at in MHz. Used to calculate phase incrument and offset
         :type Fclk: float
         """
+        self.dirty = True
         if opcode not in PulseBlaster.opcodeDict:
             raise ValueError(f"Instruction {opcode} is not a known instruction word")
         freqWord = freqWord/16 #compensate for the upscaling of 16 in the polyphase DDS
-        
         phaseIncr = round(freqWord*2**PulseBlaster.phaseWordBits/PulseBlaster.Fclk)#used to determin the frequency
         phaseOffset = round(2**PulseBlaster.phaseWordBits*phaseWord/360)
-        instructionString=format(int(phaseOffset),"030b")+format(int(phaseIncr),"030b")+format(int(ttlStates),"012b")+format(int(dataField),"020b")+format(PulseBlaster.opcodeDict[opcode],"004b")+format(int(delayCounter),"032b") 
+        instructionString=format(int(phasehopFlag),"01b")+format(int(resyncFlag),"01b")+format(int(phaseOffset),"028b")+format(int(phaseIncr),"030b")+format(int(ttlStates),"012b")+format(int(dataField),"020b")+format(PulseBlaster.opcodeDict[opcode],"004b")+format(int(delayCounter),"032b") 
         self.instruction_list.append(instructionString)
         self.num_instructions += 1 
 
@@ -58,34 +59,46 @@ class PulseBlaster(Source):
     def print_program(self):
         i = 0
         for instruction in self.instruction_list:
-            phase = int(instruction[:30],2)
+            phaseHopFlag = bool(int(instruction[0]))
+            resyncFlag = bool(int(instruction[1]))
+            phase = int(instruction[2:30],2)
             phase = phase*360/(2**PulseBlaster.phaseWordBits)
             freq = int(instruction[30:60],2)
-            freq = (freq*PulseBlaster.Fclk)/(2**PulseBlaster.phaseWordBits)
+            freq = (freq*PulseBlaster.Fclk)/(2**PulseBlaster.phaseWordBits)*16
             ttlOuts = instruction[60:72]
             data = int(instruction[72:92],2)
-            opcode = instruction[92:96]
-            delay = instruction[96:128]
-            print(f"Instruction {i}: phase = {phase}Deg, freq = {freq}MHz, ttl outputs = {ttlOuts}, data = {data}")
+            opcode = int(instruction[92:96])
+            opcode = next((k for k, v in PulseBlaster.opcodeDict.items() if v == opcode), None)
+            delay = int(instruction[96:128],2)
+            print(f"Instruction {i}: phase hop flag = {phaseHopFlag}, resync = {resyncFlag}, phase = {phase}Deg, freq = {freq}MHz, ttl outputs = {ttlOuts}, data = {data}, opcode = {opcode}, delay = {delay} clock cycles\n")
             i += 1
 
-
     def clean_program(self):
+        self.dirty = True
         self.instruction_list = []
+
+
+    def trigger(self):
+        print("PulseBlaster.trigger() is not currently implimented")
+
+    def run(self):
+        print("PulseBlaster.run() is not currently implimented")
+
+    def reset(self):
+        print("PulseBlaster.reset() is not currently implimented")
 
     def update(self):
         bytes_array = bytearray()
         for instruction in self.instruction_list:
-            lowerInstruction = int(instruction[:64],2)
-            upperInstruction = int(instruction[64:],2)
-            bytes_array += lowerInstruction.to_bytes(8, byteorder='little', signed=False)
-            bytes_array += upperInstruction.to_bytes(8, byteorder='little', signed=False)
+            for i in range(PulseBlaster.instructionLength/8): 
+                bytes_array += instruction[i*8:(i+1)*9].to_bytes(1, byteorder='little', signed=False)
+            
 
         return bytes_array, "api/pulseblaster"
 
     def __str__(self):
         output = ""
-        output += f"Number of Instructions = {self.num_instructions}"
+        output += f"[PulseBlaster] Number of Instructions = {self.num_instructions}\n"
         for port in self.ports:
             output += f"\t\t{str(port)}\n"
         return output
--- main.py
+++ main.py
@@ -15,12 +15,17 @@ def main():
     print([dac.name for dac in dacs])
     print([adc.name for adc in adcs])
 
-    awg = ArbitraryWaveformGenerator(WaveType.SINE, 1111e6, tolerance=0.1, max_samples=1e9)
+    #awg = ArbitraryWaveformGenerator(WaveType.SINE, 1111e6, tolerance=0.1, max_samples=1e9)
 
-    rf_builder.register_block(awg)
+    #rf_builder.register_block(awg)
     
     pb = PulseBlaster()
     rf_builder.register_block(pb)
+    pb.add_instruction(0,0,0,10,0b1100,0,"CONT",400*10**6)
+    pb.add_instruction(0,0,0,20,0b0100,0,"CONT",400*10**6)
+    pb.add_instruction(0,0,0,0,0b1000,0,"STOP",400*10**6)
+    pb.print_program()
+
     print(rf_builder)
     print(str(rf_builder.update()))
 
